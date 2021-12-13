d73ffff9d02d3d601401a0e8dde4ecc73c4dfde9
Jesse Brooks <brooks.jesse83@gmail.com>
2021-12-13 13:11:38 -0800

    setup routers for login

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,4 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'a/A Express Skeleton Home' });
 });
 
-router.get('/login', (req, res) => {
-  res.render('login')
-});
-
-router.post('/login', (req, res) => {
-  const { username, password } = req.body
-  console.log(`sending ${username} and ${password}`)
-  res.redirect("/")
-})
-
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login', (req, res) => {
+  res.render('login')
 });
+
+router.post('/login', (req, res) => {
+  const { username, password } = req.body
+  console.log(`sending ${username} and ${password}`)
+  res.redirect("/")
+})
 
 module.exports = router;
