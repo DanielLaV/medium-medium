6dd55989d0623c6dc48113c70aa4ddef5acbde6e
DanielLaV <djjlav@gmail.com>
2021-12-13 14:40:43 -0600

    created userValidator, csrfProtection, asyncHandler and utils

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const { csrfProtection, asyncHandler } = require('./utils');
+
+router.use(csrfProtection);
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', asyncHandler((req, res, next) => {
   res.render('index', { title: 'a/A Express Skeleton Home' });
-});
+}));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,44 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const { check, validationResult } = require('express-validator');
+const router = express.Router();
+const { csrfProtection, asyncHandler } = require('./utils');
+const db = require('../db/models');
+const { Users } = db;
+
+const userValidator = [
+  check('username')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a valid username')
+    .isLength({ max: 50 })
+    .withMessage('Username must not be more than 50 characters'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Password required')
+    .isLength({ min: 6 })
+    .withMessage('Password must be at least 6 characters long'),
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a valid first name')
+    .isLength({ max: 50 })
+    .withMessage('Name must not be more than 50 characters'),
+  check('lastName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a valid last name')
+    .isLength({ max: 50 })
+    .withMessage('Name must not be more than 50 characters'),
+]
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', asyncHandler(async (req, res, next) => {
   res.send('respond with a resource');
-});
+}));
+
+router.post('/signup', csrfProtection, userValidator, asyncHandler(async (req, res, next) => {
+
+}));
+
+router.post('/login', csrfProtection, asyncHandler(async (req, res, next) => {
+
+}));
 
 module.exports = router;
