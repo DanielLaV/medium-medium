4814b81b3f88bf44340ffa7073aced22172e377a
Nathan Treadaway <natetread@gmail.com>
2021-12-14 15:02:09 -0500

    basic html for stories and stories-new router

--- routes/stories.js
+++ routes/stories.js
@@ -5,14 +5,25 @@ const db = require("../db/models");
 const { csrfProtection, asyncHandler } = require('../utils');
 
 
-router.get("/", async (req, res, next) => {
+router.get("/", csrfProtection, async (req, res, next) => {
     const stories = await db.Story.findAll(
         { order: createdAt },
         { limit: 5 }
     )
     console.log(stories)
-    res.render("stories", { stories })
+    res.render("stories", { stories, csrfToken: req.csrfToken() })
 })
+
+router.get("/new", csrfProtection, (req, res, next) => {
+    res.render("stories-new", { csrfToken: req.csrfToken() })
+})
+
+router.post("/new", requireAuth, csrfProtection, asyncHandler(async (req, res, next) => {
+    //destructure fields
+    //add to db
+    res.render("stories-new")
+
+}))
 
 
 module.exports = router;
