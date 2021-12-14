ad197342b839d4104bbd2d35356e4f76ff938324
Chris Breen <breencf@gmail.com>
2021-12-14 12:42:30 -0500

    Implemented restore user and logout functionality

--- public/javascripts/forms.js
+++ public/javascripts/forms.js
@@ -1,39 +1,39 @@
-const login = document.querySelector('.login');
-const signup = document.querySelector('.signup')
-const loginButton = document.querySelector('#loginButton')
-const signupButton = document.querySelector('#signupButton')
+const login = document.querySelector(".login");
+const signup = document.querySelector(".signup");
+const loginButton = document.querySelector("#loginButton");
+const signupButton = document.querySelector("#signupButton");
+const logoutButton = document.querySelector("#logoutButton");
 
-
-document.body.addEventListener('click', closeLogin)
-document.body.addEventListener('click', closeSignup)
+document.body.addEventListener("click", closeLogin);
+document.body.addEventListener("click", closeSignup);
 
 function closeLogin() {
-    login.classList.remove('open')
+  login.classList.remove("open");
 }
 
 function closeSignup() {
-    signup.classList.remove('open')
+  signup.classList.remove("open");
 }
 
-loginButton.addEventListener("click", e => {
-    closeSignup()
-    e.stopPropagation()
-    login.classList.add('open')
-})
+loginButton.addEventListener("click", (e) => {
+  closeSignup();
+  e.stopPropagation();
+  login.classList.add("open");
+});
 
-login.addEventListener("click", e => {
-    closeSignup()
-    e.stopPropagation()
-})
+login.addEventListener("click", (e) => {
+  closeSignup();
+  e.stopPropagation();
+});
 
+signupButton.addEventListener("click", (e) => {
+  closeLogin();
+  e.stopPropagation();
+  signup.classList.add("open");
+});
 
-signupButton.addEventListener("click", e => {
-    closeLogin();
-    e.stopPropagation();
-    signup.classList.add('open')
-})
+signup.addEventListener("click", (e) => {
+  closeLogin();
+  e.stopPropagation();
+});
 
-signup.addEventListener("click", e => {
-    closeLogin();
-    e.stopPropagation();
-})
