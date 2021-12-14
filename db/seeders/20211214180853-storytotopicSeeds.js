29ac0b0a8efc4dc4c87c307c902d354b51dc5e77
Chris Breen <breencf@gmail.com>
2021-12-14 14:03:25 -0500

    story, STT, like, and comment topic seeds added

--- db/seeders/20211214180853-storytotopicSeeds.js
+++ db/seeders/20211214180853-storytotopicSeeds.js
@@ -1,17 +1,65 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-
-      return queryInterface.bulkInsert('StoriesToTopics', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
+    return queryInterface.bulkInsert(
+      "StoriesToTopics",
+      [
+        {
+          id: 1,
+          storyId: 1,
+          categoryId: 5,
+          createdAt: new Date(),
+          updatedAt: newDate(),
+        },
+        {
+          id: 2,
+          storyId: 1,
+          categoryId: 2,
+          createdAt: new Date(),
+          updatedAt: newDate(),
+        },
+        {
+          id: 3,
+          storyId: 2,
+          categoryId: 5,
+          createdAt: new Date(),
+          updatedAt: newDate(),
+        },
+        {
+          id: 4,
+          storyId: 3,
+          categoryId: 7,
+          createdAt: new Date(),
+          updatedAt: newDate(),
+        },
+        {
+          id: 5,
+          storyId: 4,
+          categoryId: 4,
+          createdAt: new Date(),
+          updatedAt: newDate(),
+        },
+        {
+          id: 6,
+          storyId: 5,
+          categoryId: 6,
+          createdAt: new Date(),
+          updatedAt: newDate(),
+        },
+        {
+          id: 7,
+          storyId: 2,
+          categoryId: 1,
+          createdAt: new Date(),
+          updatedAt: newDate(),
+        },
+      ],
+      {}
+    );
   },
 
   down: (queryInterface, Sequelize) => {
-
-      return queryInterface.bulkDelete('StoriesToTopics', null, {});
-
-  }
+    return queryInterface.bulkDelete("StoriesToTopics", null, {});
+  },
 };
